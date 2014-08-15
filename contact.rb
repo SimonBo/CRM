a9af309eaf09fffe4c054cca6bccfa6029c9af4a
Szymon Borucki <szymon.borucki@gmail.com>
2014-08-15 17:09:32 -0400

    Finished all the features

--- contact.rb
+++ contact.rb
@@ -15,56 +15,21 @@ class Contact
     @email=gets.chomp
     puts "Notes"
     @notes=gets.chomp 
-
-
   end
 
   def modify
     
-    puts "What do you want to modify?"
-    attribute = gets.chomp
-    puts "Are you sure you want to modify #{attribute}?"
-    answer = gets.chomp
-    if answer=='yes'
-      puts "Do you want to modify [1] ID, [2] First Name, [3] Last Name or [4] Email?"
-      variable = gets.chomp
-      until variable == 1 || variable == 2 ||variable == 3 || variable == 4
-        puts "Do you want to modify [1] ID, [2] First Name, [3] Last Name or [4] Email?"
-        variable = gets.chomp
-      end
-        case variable
-        when 1
-          puts "Current id: #{id}"
-          puts "Enter new value for id"
-          new_value=gets.chomp.to_i
-          @id=new_value
-          puts "Current id: #{id}"
-        when 2
-          puts "Current First Name: #{first_name}"
-          puts "Enter new value for First Name"
-          new_value=gets.chomp.to_i
-          @first_name=new_value
-          puts "Current First Name: #{first_name}"
-        when 3
-          puts "Current Last Name: #{last_name}"
-          puts "Enter new value for Last Name"
-          new_value=gets.chomp.to_i
-          @last_name=new_value
-          puts "Current Last Name: #{last_name}"
-        when 4
-          puts "Current Email: #{email}"
-          puts "Enter new value for Email"
-          new_value=gets.chomp.to_i
-          @email=new_value
-          puts "Current Email: #{email}"
-        end
-    else
-      crm.main_menu
-    end
-  end
-
-  def display
     
   end
+
+  def show_details
+    puts "Contact id: #{@id}"
+    puts "First Name: #{@first_name}"
+    puts "Last Name: #{@last_name}"
+    puts "Email: #{@email}"
+    puts "Notes: #{@notes}"
+    puts ""
+  end
+
 
 end
--- crm.rb
+++ crm.rb
@@ -42,19 +42,108 @@ class CRM
 
       @rolodex.add_contact(contact)
       puts "Added #{contact.id} #{contact.first_name} to rolodex"
-        
 
     elsif input ==2
-      puts "Modify"
+      puts "Whose data do you want to modify?"
+      puts "Enter First / Last Name, ID or Email"
+      variable = gets.chomp
+
+      found_contact= @rolodex.find_contact(variable)
+      found_contact.show_details
+        # puts "Are you sure you want to modify #{last_name}?"
+      
+      # answer = gets.chomp.downcase
+        # if answer=='yes'
+        puts "Do you want to modify [1] ID, [2] First Name, [3] Last Name or [4] Email?"
+        variable = gets.chomp.to_i
+          until variable == 1 || variable == 2 ||variable == 3 || variable == 4
+            puts "Invalid choice"
+            puts "Do you want to modify [1] ID, [2] First Name, [3] Last Name or [4] Email?"
+            variable = gets.chomp.to_i
+          end
+
+            case variable
+
+            when 1
+              puts "Enter new ID"
+              new_id = gets.chomp.to_i
+              found_contact.id = new_id
+              puts "ID set to #{found_contact.id}"
+
+            when 2
+              puts "Enter new first name"
+              new_first_name = gets.chomp
+              found_contact.first_name = new_first_name
+              puts "First name set to #{found_contact.first_name}"
+              
+            when 3
+              puts "Enter new Last Name"
+              new_last_name = gets.chomp
+              found_contact.last_name = new_last_name
+              puts "Last name set to #{found_contact.last_name}"
+              
+            when 4
+              puts "Enter new email"
+              new_email = gets.chomp
+              found_contact.email = new_email
+              puts "Email set to #{found_contact.email}"
+              
+            end
+        
+        
+
 
     elsif input ==3
-      puts "Display all"
+      @rolodex.show_all_contacts
+
+
     elsif input ==4
-      puts "Display one"
+      puts "Whose data do you want to view?"
+      puts "Enter First / Last Name, ID or Email"
+      variable = gets.chomp
+
+      found_contact= @rolodex.find_contact(variable)
+      found_contact.show_details
+
     elsif input ==5
-      puts "Display an attribute"
+      puts "What attribute do you want to find?"
+      puts "[1] ID, [2] First Name, [3] Last Name or [4] Email?"
+      variable = gets.chomp.to_i
+        until variable == 1 || variable == 2 ||variable == 3 || variable == 4
+          puts "Invalid choice"
+          puts "Do you want to modify [1] ID, [2] First Name, [3] Last Name or [4] Email?"
+          variable = gets.chomp.to_i
+        end
+
+        case variable
+
+        when 1
+          @rolodex.find_attribute('id')
+
+        when 2
+          @rolodex.find_attribute("first_name")
+
+        when 3
+          @rolodex.find_attribute("last_name")
+
+        when 4
+          @rolodex.find_attribute("email")
+        end
+
     elsif input ==6
-      puts "Delete"
+      puts "Which contact do you want to delete?"
+      puts "Enter First / Last Name, ID or Email"
+      variable = gets.chomp
+
+      found_contact= @rolodex.find_contact(variable)
+      found_contact.show_details
+      
+      puts "Are you sure?"
+      answer=gets.chomp.downcase
+      @rolodex.delete_contact(found_contact) if answer=='yes'
+
+
+
     elsif input ==7
       puts "Exit"
     else
